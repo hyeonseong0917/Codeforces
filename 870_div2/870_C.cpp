96a61ca00ca7b7a9f8351041e7b4263073934c5d
hyeonseong0917 <sagiri0917@gmail.com>
2023-05-12 13:29:24 +0900

    870_C

--- 870_div2/870_C.cpp
+++ 870_div2/870_C.cpp
@@ -1,35 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <math.h>
 using namespace std;
- 
-int getNum(int num){
-	
-	for(int i=2;i*i<=num;++i){
-		if(num%i==0){
-			return i;
-		}
-	}
-	return num;
-}
+
 int main() {
 	// your code goes here
 	int t=0;
 	cin>>t;
 	while(t--){
-		int n,m=0;
-		cin>>n>>m;
-		if(m==1 || n==1){
-			cout<<"YES"<<endl;
-			continue;
+		int a,b=0;
+		cin>>a>>b;
+		int N=a*b;
+		long long sum=0;
+		vector<int> v;
+		for(int i=0;i<N;++i){
+			int c=0;
+			cin>>c;
+			v.push_back(c);
 		}
-		int a=getNum(n);
-		if(a<=m){
-			cout<<"NO"<<endl;
+		sort(v.begin(),v.end());
+		int firstMin=v[0];
+		int secondMax=v[N-2];
+		int firstMax=v[N-1];
+		int secondMin=v[1];
+		int firstAns=0, secondAns=0;
+		if(a<b){
+			firstAns=abs((firstMin-secondMax)*(a-1))+abs((firstMin-firstMax)*(b-1)*a);
+			secondAns=abs((firstMax-secondMin)*(a-1))+abs((firstMax-firstMin)*(b-1)*a);
 		}else{
-			cout<<"YES"<<endl;
+			firstAns=abs((firstMin-secondMax)*(b-1))+abs((firstMin-firstMax)*(a-1)*b);
+			secondAns=abs((firstMax-secondMin)*(b-1))+abs((firstMax-firstMin)*(a-1)*b);
 		}
 		
+		sum=max(firstAns,secondAns);
+		cout<<sum<<endl;
 	}
 	return 0;
 }
