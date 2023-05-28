aa2df215bdc762dd0187dcab05dd562ff547e992
hyeonseong0917 <sagiri0917@gmail.com>
2023-05-28 16:24:04 +0900

    873_B

--- 873_div2/873_B.cpp
+++ 873_div2/873_B.cpp
@@ -2,76 +2,56 @@
 #include <algorithm>
 #include <vector>
 #include <math.h>
+#include <map>
 using namespace std;
- 
-int GCD(int a, int b){
+
+//1510
+const int MAX=100001+1;
+int gcd(int a, int b){
 	if(b==0){
 		return a;
 	}else{
-		return GCD(b,a%b);
+		return gcd(b,a%b);
 	}
 }
-int t=0;
-int N=0;
-int input_idx[100000+1];
- 
 int main() {
 	// your code goes here
+	int t=0;
 	cin>>t;
-	vector<int> arr;
-	vector<int> first_arr, second_arr;
-	for(int k=0;k<t;++k){
-		arr.clear();
-		first_arr.clear();
-		second_arr.clear();
-		cin>>N;
-		for(int i=0;i<N;++i){
+	// int arr[MAX]={0,};
+	// bool check[MAX];
+	vector<int> v;
+	while(t--){
+		int n=0;
+		cin>>n;
+		v.clear();
+		map<int,int> m;
+		
+		// arr[값]=인덱스
+		int k=1;
+		// m[k]=1;
+		for(int i=1;i<=n;++i){
 			int a=0;
 			cin>>a;
-			arr.push_back(a);
-		}
-		if(N==1){
-			cout<<0<<endl;
-			continue;
-		}
-		int mid=N/2;
-		if(N%2==0){
-			for(int i=0;i<N/2;++i){
-				first_arr.push_back(arr[i]);
-			}	
-			for(int i=N/2;i<N;++i){
-				second_arr.push_back(arr[i]);
+			int curNum=abs(a-i);
+			// if(curNum!=0){
+			// 	v.push_back(curNum);
+			// }
+			if(!m[curNum] && curNum!=0){
+				m[curNum]=1;
+				v.push_back(curNum);
 			}
-			reverse(second_arr.begin(),second_arr.end());
-		}else{
-			for(int i=0;i<N/2;++i){
-				first_arr.push_back(arr[i]);
-			}	
-			for(int i=N/2+1;i<N;++i){
-				second_arr.push_back(arr[i]);
-			}
-			reverse(second_arr.begin(),second_arr.end());
-		}
-		vector<int> diff;
-		for(int i=0;i<mid;++i){
-			diff.push_back(abs(first_arr[i]-second_arr[i]));
 		}
 		
-		int num=diff[0];
-		for(int i=1;i<mid;++i){
-			// cout<<num<<" "<<diff[i]<<endl;
-			if(num>diff[i]){
-				int res=GCD(num,diff[i]);	
-				num=res;
-			}else{
-				int res=GCD(diff[i],num);	
-				num=res;
+		sort(v.begin(),v.end());
+		k=v[0];
+		for(int i=1;i<v.size();++i){
+			if(v[i]%k!=0){
+				k=gcd(v[i],k);
 			}
-			
-			
 		}
 		
-		cout<<num<<endl;
+		cout<<k<<"\n";
 	}
 	return 0;
 }
