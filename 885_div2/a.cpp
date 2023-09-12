f012357cbf650db7072541ad117e2033daf38bad
hyeonseong0917 <sagiri0917@gmail.com>
2023-09-12 09:00:01 +0900

    885_a

--- 885_div2/a.cpp
+++ 885_div2/a.cpp
@@ -8,32 +8,40 @@ int main() {
 	int t=0;
 	cin>>t;
 	while(t--){
-		int n=0;
-		cin>>n;
-		int cnt=0;
-		vector<int> v;
-		for(int i=0;i<n;++i){
-			int a=0;
-			cin>>a;
-			v.push_back(a);
-		}
-		bool isSorted=1;
-		for(int i=0;i<n-1;++i){
-			if(v[i]>v[i+1]){
-				isSorted=0;
-				break;
+		int n,m,k;
+		int x,y;
+		cin>>n>>m>>k;
+		cin>>x>>y;
+
+		bool isCapture=0;
+		if((x+y)%2==0){
+			for(int i=0;i<k;++i){
+				int xx,yy;
+				cin>>xx>>yy;
+				if((xx+yy)%2==0){
+					isCapture=1;
+				}
+			}
+			if(isCapture==1){
+				cout<<"NO"<<"\n";
+			}else{
+				cout<<"YES"<<"\n";
+			}
+		}else{
+			for(int i=0;i<k;++i){
+				int xx,yy;
+				cin>>xx>>yy;
+				if((xx+yy)%2==1){
+					isCapture=1;
+				}
+			}
+			if(isCapture==1){
+				cout<<"NO"<<"\n";
+			}else{
+				cout<<"YES"<<"\n";
 			}
 		}
-		if(!isSorted){
-			cout<<0<<"\n";
-			continue;
-		}
-		int minNum=1222232421;
-		for(int i=0;i<n-1;++i){
-			minNum=min(minNum,v[i+1]-v[i]);
-		}
-		cout<<(minNum/2)+1<<"\n";
-		cout<<"\n";
+		
 	}
 	return 0;
 }
