5bbdc5f6c50a2c4191021dee532f5eb97bc68f2d
hyeonseong0917 <sagiri0917@gmail.com>
2023-10-19 10:52:54 +0900

    903_a

--- 903_div3/a.cpp
+++ 903_div3/a.cpp
@@ -21,70 +21,49 @@ int main() {
 	vector<char> edge;
 	vector<string> middle;
 	while(t--){
-		int n;
-		cin>>n;
-		for(int i=0;i<MAX;++i){
-			board[i]="";
-		}
-		for(int i=0;i<n;++i){
-			cin>>board[i];
-		}
-		int y=0, x=0;
+		int n,m;
+		cin>>n>>m;
+		string x,s;
+		cin>>x;
+		cin>>s;
+		// 언제 s가 x의 substring이 될까?
+		bool flag=0;
 		int cnt=0;
+		string cur_string="";
+		bool c=0;
 		while(1){
-			edge.clear();
-			middle.clear();
-			if(y==(n/2)){
+			if(flag){
 				break;
 			}
-			//edge 확인
-			edge.push_back(board[y][x]);
-			edge.push_back(board[y][n-x-1]);
-			edge.push_back(board[n-y-1][x]);
-			edge.push_back(board[n-y-1][n-x-1]);
-			sort(edge.begin(),edge.end());
-			for(int i=0;i<3;++i){
-				if(edge[i]!=edge[3]){
-					cnt+=(edge[3]-edge[i]);
+			if(x.size()>200){
+				break;
+			}
+			if(x.size()<s.size()){
+				x+=x;
+				++cnt;
+				continue;
+			}
+			for(int i=0;i<=x.size()-s.size();++i){
+				string tmp="";
+				for(int j=0;j<s.size();++j){
+					tmp+=x[i+j];
+				}
+				if(tmp.compare(s)==0){
+					flag=1;
 				}
 			}
-			string first_str="", second_str="", third_str="", fourth_str="";
-			first_str=board[y].substr(x+1,n-(2*(x+1)));
-			third_str=board[n-y-1].substr(x+1,n-(2*(x+1)));
-			reverse(third_str.begin(),third_str.end());
-			for(int i=y+1;i<n-y-1;++i){
-				second_str+=board[i][n-x-1];
-				fourth_str+=board[i][x];
+			if(!flag){
+				x+=x;
+				++cnt;
 			}
-			reverse(fourth_str.begin(),fourth_str.end());
-			middle.push_back(first_str);
-			middle.push_back(second_str);
-			middle.push_back(third_str);
-			middle.push_back(fourth_str);
-			string target="";
-			for(int i=0;i<first_str.size();++i){
-				char c='a';
-				// i번째 자리에 c를 삽입
-				for(int j=0;j<middle.size();++j){
-					// middle[j]의 i번째 숫자는 무엇인지 비교
-					if(c<middle[j][i]){
-						c=middle[j][i];
-					}
-				}
-				target+=c;
-			}
-			for(int i=0;i<middle.size();++i){
-				for(int j=0;j<middle[i].size();++j){
-					cnt+=(target[j]-middle[i][j]);
-				}
-			}
-			y+=1;
-			x+=1;
-			// break;
-			// cout<<first_str<<" "<<second_str<<" "<<third_str<<" "<<fourth_str<<endl;
-			// break;
+		
 		}
-		cout<<cnt<<"\n";
+		if(flag){
+			cout<<cnt<<"\n";	
+		}else{
+			cout<<-1<<"\n";
+		}
+		
 		
 	}	
 	return 0;
