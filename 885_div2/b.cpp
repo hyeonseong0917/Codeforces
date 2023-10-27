86d4cba156d6632d9b09dc545188f3d804fad303
hyeonseong0917 <sagiri0917@gmail.com>
2023-10-27 16:38:06 +0900

    885_b_1

--- 885_div2/b.cpp
+++ 885_div2/b.cpp
@@ -1,61 +1,66 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
+
+
+const int MAX=200000+1;
+vector<int> c;
 
 int main() {
 	// your code goes here
+	ios_base :: sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
 	int t=0;
 	cin>>t;
 	while(t--){
-		int n,k,L,R;
+		int n=0,k=0;
 		cin>>n>>k;
-		if(n%2==1){
-			L=n/2;
-			R=(n/2)+1;
-		}else{
-			L=n/2;
-			R=n/2;
+		vector<int> last(k+1,-1);
+		vector<int> fst_max(k+1,0);
+		vector<int> sed_max(k+1,0);
+		c.clear();
+		for(int i=0;i<k;++i){
+			last[i]=-1;
+			fst_max[i]=0;
+			sed_max[i]=0;
 		}
-		int cnt=0;
-		if(k==3){
-			cout<<(n/2)+1<<"\n";
-			continue;
+		
+		for(int i=0;i<n;++i){
+			int a=0;
+			cin>>a;
+			c.push_back(a);
 		}
-		while(1){
-			int cur_cnt=k-3;
-			
-			int curL=L;
-			int curR=R;
-			while(1){
-				
-				int diff=curR-curL;
-				if(diff<0){
-					break;
-				}
-				int tmp=curL;
-				curL=diff;
-				curR=tmp;
-				if(curL<0 || curL>curR){
-					break;
-				}
-				--cur_cnt;
-				if(cur_cnt==0){
-					break;
-				}
+		for(int i=0;i<n;++i){
+			if(fst_max[c[i]]<i-last[c[i]]-1){
+				sed_max[c[i]]=fst_max[c[i]];
+				fst_max[c[i]]=i-last[c[i]]-1;
+			}else if(sed_max[c[i]]<i-last[c[i]]-1){
+				sed_max[c[i]]=i-last[c[i]]-1;
 			}
-			if(cur_cnt==0){
-				++cnt;
-			}
-			--L;
-			++R;
-			if(L<R-L){
-				break;
-			}
-			// break;
+			last[c[i]]=i;
 		}
-		cout<<cnt<<"\n";
-		// cout<<L<<" "<<R<<" "<<cnt<<endl;
+
+		for(int i=1;i<=k;++i){
+			if(fst_max[i]<n-last[i]-1){
+				sed_max[i]=fst_max[i];
+				fst_max[i]=n-last[i]-1;
+
+			}else if(sed_max[i]<n-last[i]-1){
+				sed_max[i]=n-last[i]-1;
+			}
+		}
+		int ans=1222232421;
+		for(int i=1;i<=k;++i){
+			if(last[i]==-1) continue;
+			int cur_num=max(fst_max[i]/2,sed_max[i]);
+			// cout<<i<<" "<<fst_max[i]<<" "<<sed_max[i]<<endl;
+			ans=min(ans,cur_num);
+		}
+		cout<<ans<<"\n";
+		// cout<<min_num_color<<" "<<min_max_num;		
 	}
 	return 0;
 }
