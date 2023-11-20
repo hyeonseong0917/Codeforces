1bf5d67ec8bf554468606ed95c839860d5b40e12
hyeonseong0917 <sagiri0917@gmail.com>
2023-11-20 13:37:42 +0900

    155_B

--- edu_155_div2/155_A.cpp
+++ edu_155_div2/155_A.cpp
@@ -3,22 +3,35 @@
 using namespace std;
 
 //1555
+#define ll long long
 int main() {
 	// your code goes here
 	int t=0;
 	cin>>t;
 	while(t--){
-		int x,y,k;
-		cin>>x>>y>>k;
-		if(x>=y){
-			cout<<x<<"\n";
-		}else{
-			if(x+k>=y){
-				cout<<y<<"\n";
+		int n=0;
+		cin>>n;
+		bool flag=0;
+		ll cur_s=0;
+		ll cur_e=0;
+		for(int i=0;i<n;++i){
+			ll a,b;
+			cin>>a>>b;
+			if(i==0){
+				cur_s=a;
+				cur_e=b;
 			}else{
-				cout<<y+y-(x+k)<<"\n";
+				if(cur_s<=a && cur_e<=b){
+					flag=1;
+				}
 			}
-		}		
+		}
+		if(flag){
+			cout<<-1<<"\n";
+		}else{
+			cout<<cur_s<<"\n";
+		}
+		
 	}
 	return 0;
 }
