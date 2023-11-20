7ad35cec932e64dfae5ec5a974fda1ec4377e1c7
hyeonseong0917 <sagiri0917@gmail.com>
2023-11-20 16:32:41 +0900

    152_A

--- edu_152_div2/152_A.cpp
+++ edu_152_div2/152_A.cpp
@@ -9,27 +9,12 @@ int main() {
 	int t=0;
 	cin>>t;
 	while(t--){
-		int n=0;
-		cin>>n;
-		bool flag=0;
-		ll cur_s=0;
-		ll cur_e=0;
-		for(int i=0;i<n;++i){
-			ll a,b;
-			cin>>a>>b;
-			if(i==0){
-				cur_s=a;
-				cur_e=b;
-			}else{
-				if(cur_s<=a && cur_e<=b){
-					flag=1;
-				}
-			}
-		}
-		if(flag){
-			cout<<-1<<"\n";
+		int b,c,h;
+		cin>>b>>c>>h;
+		if(c+h+1>=b){
+			cout<<b*2-1<<"\n";
 		}else{
-			cout<<cur_s<<"\n";
+			cout<<(c+h)*2+1<<"\n";
 		}
 		
 	}
