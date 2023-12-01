868aa38edae38ead4a65f10f326557561c01d4ab
hyeonseong0917 <sagiri0917@gmail.com>
2023-12-01 14:04:28 +0900

    145_B

--- edu_145_div2/145_B.cpp
+++ edu_145_div2/145_B.cpp
@@ -1,47 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <cmath>
 using namespace std;
 //1017
+#define ll long long
 int main() {
 	// your code goes here
 	int t=0;
 	cin>>t;
-	vector<long long> a;
-	vector<long long> psum(200000+1,0);
 	while(t--){
-		int n,k,ok=0;
-		cin>>n>>k;
-		a.clear();
-		psum.clear();
-		
-		for(int i=0;i<n;++i){
-			long long c=0;
-			cin>>c;
-			a.push_back(c);
-		}
-		
-		sort(a.begin(),a.end());
-		psum[0]=a[0];
-		for(int i=1;i<n;++i){
-			psum[i]=psum[i-1]+a[i];
-		}
-		long long maxNum=0;
-		//앞에서 m*2개, 뒤에서 k-m개 삭제
-		// 1 2 5 6 10
-		for(int m=0;m<=k;++m){
-			// cout<<m<<"\n";
-			if(m==0){
-				maxNum=psum[n-1-k];
-				// cout<<maxNum;
+		ll n;
+		cin>>n;
+		// 각 pair의 거리 차이가 1보다 크면서 (x,y)합의 최댓값의 최소를 찾기
+		ll L=0, R=1000000000;
+		ll ans=R;
+		while(L<=R){
+			ll mid=(L+R)/2;
+			ll cur_num=(mid+1)*(mid+1);
+			if(cur_num>=n){
+				ans=min(ans,mid);
+				R=mid-1;
 			}else{
-				// cout<<m*2<<endl;
-				long long first_sum=psum[(m*2)-1];
-				long long second_sum=psum[n-1-(k-m)];
-				maxNum=max(maxNum,second_sum-first_sum);
-			}	
+				L=mid+1;
+			}
 		}
-		cout<<maxNum<<"\n";
+		cout<<ans<<"\n";
+		
 	}
 	return 0;
 }
