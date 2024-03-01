75e9dfd8aab33ccac6739891f2e70f603f298b7b
hyeonseong0917 <sagiri0917@gmail.com>
2024-03-01 17:31:11 +0900

    924_c

--- 924_div2/c.cpp
+++ 924_div2/c.cpp
@@ -1,62 +1,67 @@
-	#include <iostream>
-	#include <vector>
-	#include <set>
-	#include <map>
-	#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+using namespace std;
 
-	using namespace std;
+#define ll long long
 
-	#define ll long long
-
-	const int MAX=1000+1;
-
-
-	ll gcd(ll a, ll b){
-		if(b==0){
-			return a;
-		}else{
-			return gcd(b,a%b);
-		}
-
-	}
-	int main() {
-		// your code goes here
-		ios_base :: sync_with_stdio(false); 
-		cin.tie(NULL); 
-		cout.tie(NULL);
-		int t=0;
-		cin>>t;
-		map<ll,ll> mm;
-		ll cur_num=1;
-		while(1){
-			if(cur_num>1000000000){
-				break;
-			}
-			mm[cur_num]=1;
-			cur_num*=2;
-		}
-		// cout<<m[1024];
-		while(t--){
-			ll n,m;
-			cin>>n>>m;
-			n=n%m;
-			if(n==0 || m==1){
-				cout<<0<<"\n";
-				continue;
-			}
-			if(m%2==1 || !mm[m/gcd(m,n)]){
-				cout<<-1<<"\n";
-				continue;
-			}
-			ll ans=0;
-			while(n){
-				ans+=n;
-				n*=2;
-				if(n>=m){
-					n-=m;
+int main() {
+	// your code goes here
+	ios_base :: sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	int t=0;
+	cin>>t;
+	while(t--){
+		ll n,x;
+		cin>>n>>x;
+		map<ll,ll> m;
+		ll k=0;
+		k=max(x,(ll)2);
+		ll cnt=0;
+		if((n-x)%2==0){
+			ll fst_num=(n-x)/2;	
+			for(ll t=1;t<=(ll)sqrt(fst_num);++t){
+				if(fst_num%t==0){
+					if(!m[t] && t>=x-1){
+						// cout<<t<<endl;
+						m[t]=1;
+						++cnt;	
+					}
+					if(!m[fst_num/t] && fst_num/t>=x-1){
+						// cout<<fst_num/t<<endl;
+						++m[fst_num/t];
+						++cnt;
+					}
 				}
 			}
-			cout<<ans<<"\n";
-		}	
-		return 0;
-	}
+		}
+		if(x==1){
+			cout<<cnt<<"\n";
+			continue;
+		}
+		if((n+x-2)%2==0){
+			ll sed_num=(n+x-2)/2;	
+			for(ll t=1;t<=(ll)sqrt(sed_num);++t){
+				if(sed_num%t==0){
+					if(!m[t] && t>=x-1){
+						// cout<<t<<endl;
+						m[t]=1;
+						++cnt;	
+					}
+					if(!m[sed_num/t] && sed_num/t>=x-1){
+						// cout<<sed_num/t<<endl;
+						++m[sed_num/t];
+						++cnt;
+					}
+				}
+			}
+		}
+		cout<<cnt<<"\n";
+
+	}	
+	return 0;
+}
