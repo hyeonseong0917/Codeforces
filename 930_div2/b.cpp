10993ed4d72f4cd21a82041bdf969159bad4c017
hyeonseong0917 <sagiri0917@gmail.com>
2024-03-02 12:20:42 +0900

    922_b

--- 930_div2/b.cpp
+++ 930_div2/b.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 #define ll long long
 
+
 int main() {
 	// your code goes here
 	ios_base :: sync_with_stdio(false); 
@@ -15,37 +16,27 @@ int main() {
 	cout.tie(NULL);
 	int t=0;
 	cin>>t;
-	char board[2][200000+1];
 	while(t--){
 		int n;
 		cin>>n;
-		for(int i=0;i<2;++i){
-			for(int j=0;j<n;++j){
-				cin>>board[i][j];
-			}
+		vector<pair<int,int>> v;
+		for(int i=0;i<n;++i){
+			int a=0;
+			cin>>a;
+			v.push_back({a,0});
 		}
-		int L=0;
-		int R=n-1;
-		string s="";
-		for(int i=n-1;i>=1;--i){
-			if(board[0][i]=='1' && board[1][i-1]=='0'){
-				R=i-1;
-			}
+		for(int i=0;i<n;++i){
+			int a=0;
+			cin>>a;
+			v[i].second=a;
 		}
-		for(int i=0;i<R;++i){
-			if(board[0][i+1]=='0' && board[1][i]=='1'){
-				L=i+1;
-			}
-		}
-		// cout<<L<<" "<<R<<"\n";
-		for(int i=0;i<=L;++i){
-			s+=board[0][i];
-		}
-		for(int i=L;i<n;++i){
-			s+=board[1][i];
-		}
-		cout<<s<<"\n";
-		cout<<R-L+1<<"\n";
+		sort(v.begin(),v.end());
+		for(int i=0;i<n;++i){
+			cout<<v[i].first<<" ";
+		}cout<<"\n";
+		for(int i=0;i<n;++i){
+			cout<<v[i].second<<" ";
+		}cout<<"\n";
 	}	
 	return 0;
 }
