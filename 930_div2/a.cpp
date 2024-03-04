722760905cfdb47c8e9865df3b51cc3906b5cacf
hyeonseong0917 <sagiri0917@gmail.com>
2024-03-04 21:01:49 +0900

    930_A

--- 930_div2/a.cpp
+++ 930_div2/a.cpp
@@ -1,48 +1,29 @@
 #include <iostream>
-#include <vector>
-#include <set>
-#include <map>
-#include <algorithm>
-
+#include <cmath>
 using namespace std;
 
 #define ll long long
-
 int main() {
 	// your code goes here
-	ios_base :: sync_with_stdio(false); 
-	cin.tie(NULL); 
-	cout.tie(NULL);
 	int t=0;
 	cin>>t;
 	while(t--){
-		ll a,b,n;
-		cin>>a>>b>>n;
-		vector<ll> v;
-		for(int i=0;i<n;++i){
-			ll k=0;
-			cin>>k;
-			v.push_back(k);
-		}	
-		sort(v.begin(),v.end());
-		ll cur_time=0;
-		int idx=0;
+		ll N;
+		cin>>N;
+		ll a=1;
+		ll cnt=0;
 		while(1){
-			if(idx==n){
-				cur_time+=1;
+			a=(ll)pow(2,cnt);
+			if(a>=N){
+				if(a!=N){
+					--cnt;
+				}
 				break;
+			}else{
+				++cnt;
 			}
-			if(b==1 && idx<n){
-				b+=v[idx];
-				b=min(a,b);
-				++idx;
-			}
-			cur_time+=(b-1);
-			b=1;
 		}
-		cout<<cur_time<<"\n";
-
-		
-	}	
+		cout<<(ll)pow(2,cnt)<<"\n";
+	}
 	return 0;
 }
