1e2977bc2b3b3b7603cdb4af275d52f0fa5e1f78
hyeonseong0917 <sagiri0917@gmail.com>
2024-03-10 14:29:42 +0900

    925D

--- 925_div3/d.cpp
+++ 925_div3/d.cpp
@@ -6,12 +6,8 @@
 #include <set>
 using namespace std;
 
-bool comp(pair<int,pair<int,int>> a, pair<int,pair<int,int>> b){
-	if(a.second.second==b.second.second){
-		return a.second.first<b.second.first;
-	}
-	return a.second.second<b.second.second;
-}
+#define ll long long
+
 int main() {
 	// your code goes here
 	ios_base :: sync_with_stdio(false); 
@@ -20,93 +16,36 @@ int main() {
 	int t=0;
 	cin>>t;
 	while(t--){
-		int n,Q;
-		cin>>n;
-		vector<int> v;
-		map<int,int> is_same;
-		int cnt=0;
-		for(int i=0;i<n;++i){
-			int a=0;
+		ll N,X,Y;
+		cin>>N>>X>>Y;
+		vector<ll> v;
+		map<pair<ll,ll>,ll> m;
+		for(ll i=0;i<N;++i){
+			ll a=0;
 			cin>>a;
-			if(!is_same[a]){
-				++is_same[a];
-				++cnt;
-			}
 			v.push_back(a);
+			++m[{a%X,a%Y}];
 		}
-		cin>>Q;
-		vector<pair<int,pair<int,int>>> q;
-		for(int i=0;i<Q;++i){
-			int a,b;
-			cin>>a>>b;
-			q.push_back({i,{a-1,b-1}});
-		}
-		if(cnt==1){
-			for(int i=0;i<Q;++i){
-				cout<<-1<<" "<<-1<<"\n";
-			}cout<<"\n";
-			continue;
-		}
-		sort(q.begin(),q.end(),comp);
-		map<int,pair<int,int>> ans;
-		map<int,int> m;
-		for(int i=0;i<Q;++i){
-			int cur_idx=q[i].first;
-			int start_idx=q[i].second.first;
-			int final_idx=q[i].second.second;
-			if(v[start_idx]!=v[final_idx]){
-				ans[cur_idx]={start_idx,final_idx};
-				m[v[start_idx]]=final_idx;
-			}else{
-				if(m[v[start_idx]]){
-					if(start_idx<m[v[start_idx]] && m[v[start_idx]]<final_idx){
-						// if(start_idx==1 && final_idx==3){
-						// 	cout<<"he"<<" "<<m[v[start_idx]]<<" "<<"\n";
-						// }
-						ans[cur_idx]={start_idx,m[v[start_idx]]};
-					}else{
-						int L=start_idx;
-						int R=final_idx;
-						while(L<R){
-							if(v[L]!=v[R]){
-								m[v[start_idx]]=R;
-								break;
-							}
-							--R;
-						}
-						if(L==R){
-							ans[cur_idx]={-1,-1};
-						}else{
-							ans[cur_idx]={start_idx,R};
-						}
-					}
-				}else{
-					int L=start_idx;
-					int R=final_idx;
-					while(L<R){
-						if(v[L]!=v[R]){
-							m[v[start_idx]]=R;
-							break;
-						}
-						--R;
-					}
-					if(L==R){
-						ans[cur_idx]={-1,-1};
-						
-					}else{
-						ans[cur_idx]={start_idx,R};
-					}
+		ll cnt=0;
+		for(ll i=0;i<N;++i){
+			ll cur_num=v[i];
+			if(cur_num%X==(X-cur_num%X)%X){
+				if(m[{cur_num%X,cur_num%Y}]){
+					cnt+=m[{cur_num%X,cur_num%Y}]-1;
+					--m[{cur_num%X,cur_num%Y}];
 				}
-			}
-		}
-		for(int i=0;i<Q;++i){
-			if(ans[i].first!=-1 && ans[i].second!=-1){
-				cout<<ans[i].first+1<<" "<<ans[i].second+1<<"\n";
 			}else{
-				cout<<ans[i].first<<" "<<ans[i].second<<"\n";	
+				if(m[{cur_num%X, cur_num%Y}] && m[{(X-cur_num%X)%X,cur_num%Y}]){
+					cnt+=m[{cur_num%X, cur_num%Y}]*m[{(X-cur_num%X)%X,cur_num%Y}];
+					m[{(X-cur_num%X)%X,cur_num%Y}]=0;
+					m[{cur_num%X, cur_num%Y}]=0;
+				}	
 			}
+			// cout<<i<<" "<<cnt<<"\n";	
 			
-		}cout<<"\n";
+		}
+		
+		cout<<cnt<<"\n";
 	}
 	return 0;
 }
