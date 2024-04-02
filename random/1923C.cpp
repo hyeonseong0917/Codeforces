2bd773c82e5174c696c6a038b32e2eb8ccd022f6
hyeonseong0917 <sagiri0917@gmail.com>
2024-04-03 00:19:47 +0900

    162C

--- random/1923C.cpp
+++ random/1923C.cpp
@@ -14,21 +14,24 @@ int main() {
 		vector<ll> v(n,0);
 		//dp[i]: i인덱스까지 1의 개수
 		vector<ll> one_cnt(n,0);
-		vector<ll> psum(n,0);
+		vector<ll> psum(n,0); //1 제외한 sum
 		ll cur_num=0;
 		for(ll i=0;i<n;++i){
 			cin>>v[i];
 		}
 		if(v[0]==1){
 			++one_cnt[0];
+		}else{
+			psum[0]=v[0];
 		}
-		psum[0]=v[0];
 		for(ll i=1;i<n;++i){
 			one_cnt[i]=one_cnt[i-1];
-			psum[i]=psum[i-1]+v[i];
+			psum[i]=psum[i-1];
 			if(v[i]==1){
 				++one_cnt[i];
 				// cout<<i<<" "<<one_cnt[i]<<endl;
+			}else{
+				psum[i]+=v[i];
 			}
 		}
 		for(ll i=0;i<q;++i){
@@ -39,17 +42,13 @@ int main() {
 			ll cur_sum=psum[b];
 			ll cur_one_cnt=one_cnt[b];
 			// cout<<cur_one_cnt<<"\n";
-			ll cur_rest_cnt=(b-a+1)-cur_one_cnt;
 			if(a!=0){
 				cur_sum-=psum[a-1];
 				cur_one_cnt-=one_cnt[a-1];
 			}
-			if(a==b){
-				cout<<"NO"<<"\n";
-				continue;
-			}
+			ll cur_rest_cnt=(b-a+1)-cur_one_cnt;
 			// cout<<cur_sum<<" "<<cur_rest_cnt<<" "<<cur_one_cnt<<"\n";
-			if(cur_sum>=cur_one_cnt+(b-a+1)){
+			if(cur_sum-cur_rest_cnt>=cur_one_cnt && a!=b){
 				cout<<"YES"<<"\n";
 			}else{
 				cout<<"NO"<<"\n";
