b84a112c9df0e6959803c39f4a33efb7b7439658
hyeonseong0917 <sagiri0917@gmail.com>
2024-04-30 22:31:33 +0900

    928B

--- 928_div4/b.cpp
+++ 928_div4/b.cpp
@@ -16,15 +16,34 @@ int main() {
 	int t=0;
 	cin>>t;
 	while(t--){
-		ll n,k;
-		cin>>n>>k;
-		ll ans;
-		if(k<=(2*n-2)*2){
-			ans=(k+1)/2;
-		}else{
-			ans=2*n-2+(k-(2*n-2)*2);
+		int n;
+		vector<string> s;
+		int one_cnt=0;
+		cin>>n;
+		for(int i=0;i<n;++i){
+			string t;
+			cin>>t;
+			s.push_back(t);
 		}
-		cout<<ans<<"\n";
+		
+		bool is_square=1;
+		for(int i=0;i<n-1;++i){
+			for(int j=0;j<n-1;++j){
+				int one_cnt=0;
+				if(s[i][j]=='1') ++one_cnt;
+				if(s[i][j+1]=='1') ++one_cnt;
+				if(s[i+1][j]=='1')  ++one_cnt;
+				if(s[i+1][j+1]=='1') ++one_cnt;
+				if(one_cnt==3){
+					is_square=0;
+				}
+			}
+		}
+		if(is_square){
+			cout<<"SQUARE"<<"\n";
+		}else{
+			cout<<"TRIANGLE"<<"\n";
+		}
 	}	
 	return 0;
 }
