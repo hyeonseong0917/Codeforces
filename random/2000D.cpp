83c14d82084c3981f47fb91daff41468da1be643
Hyeonseong0917 <sagiri0917@gmail.com>
2024-12-21 17:06:44 +0900

    2000D

--- random/2000D.cpp
+++ random/2000D.cpp
@@ -20,12 +20,18 @@ int main() {
 		ll n;
 		cin>>n;
 		vector<ll> v(n,0);
+		vector<ll> psum(n,0);
 		string s;
 		for(ll i=0;i<n;++i){
 			cin>>v[i];
+			if(i==0){
+				psum[i]=v[i];
+			}else{
+				psum[i]=psum[i-1]+v[i];
+			}
 		}
 		cin>>s;
-		ll min_l,max_r;
+		ll min_l=-1,max_r=-1;
 		for(ll i=0;i<n;++i){
 			if(s[i]=='L'){
 				min_l=i;
@@ -38,10 +44,30 @@ int main() {
 				break;
 			}
 		}
-		ll L=min_l+1, R=min_l+1;
-		while(L<=R && L<max_r && R<max_r){
-			
+		if(min_l==-1 || max_r==-1 || max_r<min_l){
+			cout<<0<<"\n";
+			continue;
 		}
+		ll ans=psum[max_r]-psum[min_l]+v[min_l];
+		ll L=min_l+1, R=max_r-1;
+		while(L<R){
+			if(s[L]=='L'){
+				while(L<R){
+					if(s[R]=='R'){
+						break;
+					}
+					--R;
+				}
+				if(L<R){
+					ans+=psum[R]-psum[L]+v[L];
+				}
+				++L;
+				--R;
+			}else{
+				++L;
+			}
+		}
+		cout<<ans<<"\n";
 
 	}
 	return 0;
