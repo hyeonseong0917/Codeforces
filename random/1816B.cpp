1981423625e0bcd486f5a468ad39a238987f6a53
Hyeonseong0917 <sagiri0917@gmail.com>
2025-02-13 14:18:22 +0900

    1816B

--- random/1816B.cpp
+++ random/1816B.cpp
@@ -15,7 +15,35 @@ int main() {
 	int t=1;
 	cin>>t;
 	while(t--){
-		
+		ll n;
+		cin>>n;
+		vector<vector<ll>> v;
+		for(ll i=0;i<2;++i){
+			vector<ll> tmp;
+			for(ll j=0;j<n;++j){
+				tmp.push_back(0);
+			}
+			v.push_back(tmp);
+		}
+		v[0][0]=2*n;
+		v[1][n-1]=2*n-1;
+		ll min_num=1;
+		ll max_num=2*n-2;
+		for(ll i=0;i<n-1;++i){
+			if(i%2==0){
+				v[1][i]=min_num++;
+				v[0][i+1]=min_num++;
+			}else{
+				v[0][i+1]=max_num--;
+				v[1][i]=max_num--;
+				
+			}
+		}
+		for(ll i=0;i<2;++i){
+			for(ll j=0;j<n;++j){
+				cout<<v[i][j]<<" ";
+			}cout<<"\n";
+		}
 	} 	
 
 	return 0;
