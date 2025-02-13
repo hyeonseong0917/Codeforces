17b5cea2506a855c3db934062037ba6d39b8f5e5
Hyeonseong0917 <sagiri0917@gmail.com>
2025-02-13 13:21:28 +0900

    1838C

--- random/1838B.cpp
+++ random/1838B.cpp
@@ -15,7 +15,40 @@ int main() {
 	int t=1;
 	cin>>t;
 	while(t--){
-		
+		ll n;
+		cin>>n;
+		vector<ll> v(n,0);
+		vector<ll> check(n+1,0);
+		for(ll i=0;i<n;++i){
+			cin>>v[i];
+			check[v[i]]=i;
+		}
+		if(v[0]==1 || v[n-1]==1){
+			ll sed_idx=check[2];
+			if(v[0]==1){
+				cout<<sed_idx+1<<" "<<n<<"\n";
+			}else{
+				cout<<sed_idx+1<<" "<<1<<"\n";
+			}
+		}else{
+			// 1을 사이드로 보내야함
+			ll fst_idx=check[1];
+			ll sed_idx=check[2];
+			ll n_idx=check[n];
+			ll L=min(fst_idx,sed_idx);
+			ll R=max(fst_idx,sed_idx);
+			if(L<n_idx && n_idx<R){
+				cout<<1<<" "<<1<<"\n";
+			}else{
+				
+				if(n_idx<L){
+					cout<<L+1<<" "<<n_idx+1<<"\n";
+				}else{
+					cout<<R+1<<" "<<n_idx+1<<"\n";
+				}
+			}
+
+		}
 	} 	
 
 	return 0;
--- random/1838C.cpp
+++ random/1838C.cpp
@@ -15,7 +15,55 @@ int main() {
 	int t=1;
 	cin>>t;
 	while(t--){
-		
+		ll n,m;
+		cin>>n>>m;
+		bool is_prime=1;
+		for(ll i=2;i<=sqrt(m);++i){
+			if(m%i==0){
+				is_prime=0;
+				break;
+			}
+		}
+		if(!is_prime){
+			ll cnt=1;
+			for(ll i=0;i<n;++i){
+				for(ll j=0;j<m;++j){
+					cout<<cnt<<" ";
+					++cnt;
+				}cout<<"\n";
+			}
+		}else{
+			ll cnt=1;
+			vector<vector<ll>> v;
+			for(ll i=0;i<n;++i){
+				vector<ll> tmp;
+				for(ll j=0;j<m;++j){
+					tmp.push_back(cnt++);
+				}
+				v.push_back(tmp);
+			}
+			// 1 2 3 4 5
+			// 6 7 8 9 10
+			// 11 12 13 14 15
+			// 16 17 18 19 20
+			// 21 22 23 24 25
+
+			// 홀수번째 -> 짝수번째
+			for(ll i=0;i<n;++i){
+				if(i%2==1){
+					for(ll j=0;j<m;++j){
+						cout<<v[i][j]<<" ";
+					}cout<<"\n";
+				}
+			}
+			for(ll i=0;i<n;++i){
+				if(i%2==0){
+					for(ll j=0;j<m;++j){
+						cout<<v[i][j]<<" ";
+					}cout<<"\n";
+				}
+			}
+		}
 	} 	
 
 	return 0;
