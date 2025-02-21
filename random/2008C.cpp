670b39028e097e4969a5622fa538f1f52534e72f
Hyeonseong0917 <sagiri0917@gmail.com>
2025-02-21 12:05:50 +0900

    2008C

--- random/2008C.cpp
+++ random/2008C.cpp
@@ -15,7 +15,25 @@ int main() {
 	int t=1;
 	cin>>t;
 	while(t--){
-		
+		ll L,R;
+		cin>>L>>R;
+		ll ans=0;
+		if(L==R){
+			cout<<1<<"\n";
+			continue;
+		}
+		ll lx=1, rx=100000;
+		ll max_idx=-1;
+		while(lx<=rx){
+			ll mid=(lx+rx)/2;
+			if(L+mid*(mid+1)/2<=R){
+				max_idx=max(max_idx,mid);
+				lx=mid+1;
+			}else{
+				rx=mid-1;
+			}
+		}
+		cout<<max_idx+1<<"\n";
 	} 	
 
 	return 0;
