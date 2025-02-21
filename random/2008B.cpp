27f4f426a65f1ebbb61a4905bd64e3f931a27a41
Hyeonseong0917 <sagiri0917@gmail.com>
2025-02-21 12:02:09 +0900

    2008B

--- random/2008B.cpp
+++ random/2008B.cpp
@@ -15,7 +15,37 @@ int main() {
 	int t=1;
 	cin>>t;
 	while(t--){
-		
+		ll n;
+		cin>>n;
+		string s;
+		cin>>s;
+		bool is_exist_zero=0;
+		for(ll i=0;i<n;++i){
+			if(s[i]=='0'){
+				is_exist_zero=1;
+			}
+		}
+		if(!is_exist_zero){
+			if(n==4){
+				cout<<"YES"<<"\n";
+			}else{
+				cout<<"NO"<<"\n";
+			}
+			continue;
+		}
+		ll idx=0;
+		for(ll i=0;i<n;++i){
+			if(s[i]=='0'){
+				idx=i;
+				break;
+			}
+		}
+		--idx;
+		if(idx*idx==n){
+			cout<<"YES"<<"\n";
+		}else{
+			cout<<"NO"<<"\n";
+		}
 
 	} 	
 
