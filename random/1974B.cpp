52f69eb74c848393d4b53220354875d3524cb55b
Hyeonseong0917 <sagiri0917@gmail.com>
2025-02-24 14:15:18 +0900

    1974B

--- random/1974B.cpp
+++ random/1974B.cpp
@@ -11,7 +11,46 @@ int main() {
 	int t=0;
 	cin>>t;
 	while(t--){
-		
+		// s
+        // 새로운 string r을 구성 => s의 고유한 문자들을 사전순으로
+        // encoding된 string이 주어질 때 decoding된 스트링 구하기
+        ll n;
+        cin>>n;
+        string s;
+        cin>>s;
+        // serofedsoc
+        // 1. disctinct string: serofdc
+        // 2. sort: cdefors
+        // 3. s->c, c->s
+        vector<ll> check(26+1,0);
+        for(ll i=0;i<n;++i){
+            ++check[s[i]-'a'];
+        }
+        string r="";
+        for(ll i=0;i<26;++i){
+            if(check[i]){
+                r+=('a'+i);
+            }
+        }
+        // cout<<r<<endl;
+        map<char,char> m;
+        ll L=0, R=r.size()-1;
+        while(L<=R){
+            if(L==R){
+                m[r[L]]=r[L];
+                break;
+            }
+            m[r[L]]=r[R];
+            m[r[R]]=r[L];
+            ++L;
+            --R;
+        }
+        // cout<<s<<endl;
+        for(ll i=0;i<n;++i){
+            s[i]=m[s[i]];
+            // cout<<m[s[i]]<<endl;
+        }
+        cout<<s<<"\n";
 	} 
 	return 0;
 }
