888d7df6578a9a5d0e235bd01949ab4eb32a1519
Hyeonseong0917 <sagiri0917@gmail.com>
2025-02-25 16:22:27 +0900

    1833C

--- random/1833B.cpp
+++ random/1833B.cpp
@@ -11,7 +11,29 @@ int main() {
 	int t=0;
 	cin>>t;
 	while(t--){
-		
+		ll n,k;
+		cin>>n>>k;
+		vector<ll> a(n,0),b(n,0);
+		vector<pair<ll,ll>> av,bv;
+		for(ll i=0;i<n;++i){
+			cin>>a[i];
+			av.push_back({a[i],i});
+		}
+		for(ll i=0;i<n;++i){
+			cin>>b[i];
+			bv.push_back({b[i],i});
+		}
+		sort(av.begin(),av.end());
+		sort(b.begin(),b.end());
+		// |a[i]-b[i]|<=k
+		vector<ll> ans(n,0);
+		for(ll i=0;i<n;++i){
+			ll a_idx=av[i].second;
+			ans[a_idx]=b[i];
+		}
+		for(ll i=0;i<n;++i){
+			cout<<ans[i]<<" ";
+		}cout<<"\n";
 	} 
 	return 0;
 }
