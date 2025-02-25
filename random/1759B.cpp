b651604357b64e3d554d20c57dfd69e769d1b4c5
Hyeonseong0917 <sagiri0917@gmail.com>
2025-02-25 16:53:57 +0900

    1759B

--- random/1759B.cpp
+++ random/1759B.cpp
@@ -11,61 +11,38 @@ int main() {
 	int t=0;
 	cin>>t;
 	while(t--){
-		ll n;
-		cin>>n;
-		vector<ll> v(n,0);
-		// b[i]에 대해 a[i]혹은 a[i]-a[j]를 할당 가능
-		for(ll i=0;i<n;++i){
+		ll m,s;
+		cin>>m>>s;
+		vector<ll> v(m,0);
+		ll cur_sum=0;
+		for(ll i=0;i<m;++i){
 			cin>>v[i];
+			cur_sum+=v[i];
 		}
 		sort(v.begin(),v.end());
-		// 2 3 4 6 8
-		// 1 4 6 7 9
-		vector<ll> odd_dp(n,0), even_dp(n,0);
-		// odd_dp[i]: i인덱스까지 홀수가 있는지?
-		// even_dp[i]: i인덱스까지 짝수가 있는지?
-		if(v[0]%2==0){
-			even_dp[0]=1;
-		}else{
-			odd_dp[0]=1;
-		}
-		for(ll i=1;i<n;++i){
-			if(v[i]%2==1){
-				odd_dp[i]=1;
-			}else{
-				odd_dp[i]=odd_dp[i-1];
-			}
-		}
-		for(ll i=1;i<n;++i){
-			if(v[i]%2==0){
-				even_dp[i]=1;
-			}else{
-				even_dp[i]=even_dp[i-1];
-			}
-		}
+		ll max_num=v[m-1];
+		ll diff=((max_num*(max_num+1))/2-cur_sum);
+		s-=diff;
+		// 2 1
+		// 4 3
 		bool flag=0;
-		if(v[0]%2==1){
-			// 홀수로 맞춰야함
-			for(ll i=1;i<n;++i){
-				if(v[i]%2==1) continue;
-				if(!odd_dp[i-1]){
-					flag=1;
-					break;
-				}
+		for(ll i=max_num+1;i<=1000;++i){
+			if(s==0){
+				flag=1;
+				break;
 			}
-		}else{
-			for(ll i=1;i<n;++i){
-				if(v[i]%2==0) continue;
-				if(!odd_dp[i-1]){
-					flag=1;
-					break;
-				}
+			s-=i;
+			if(s<0){
+				break;
 			}
+		}
+		if(s==0){
+			flag=1;
 		}
 		if(flag){
-			cout<<"NO"<<"\n";
-		}else{
 			cout<<"YES"<<"\n";
+		}else{
+			cout<<"NO"<<"\n";
 		}
 	} 
 	return 0;
